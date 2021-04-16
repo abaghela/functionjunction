d6a2eaac97100e33467767b48afc4066c4bb4d24
Arjun Baghela <arjunsbaghela@gmail.com>
2021-04-15 22:43:25 -0700

    updated first_five

--- R/first_five.R
+++ R/first_five.R
@@ -12,11 +12,18 @@
 #' @export
 
 first_five <- function(mat, rows = 5, columns = 5) {
+
+  # Confirm this is a matrix or dataframe
+  if (!(is.matrix(mat) | is.data.frame(mat))) {
+    return(cat("This ain't a matrix or dataframe.") )
+  }
+
   mat <- as.data.frame(mat)
 
   mat_rows = nrow(mat)
   mat_cols = ncol(mat)
 
+  # Are there fewer rows/columnns than requested?
   if (mat_rows < rows){
     rows = mat_rows
   }
